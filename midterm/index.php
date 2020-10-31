e5a0b28d49e101d5411e4a4771f174567cd051f7
aw0813 <aw0813@naver.com>
2020-10-31 14:36:55 +0000

    add midterm

---
+++ midterm/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> World DataBase </title>
+</head>
+<body>
+    <h1>세계 정보</h1>
+    <a href = "emp_select.php">(1) 수도와 인구수</a> <br>
+    <a href = "emp_insert.php">(2) 신규 직원 등록</a> <br>
+   
+
+</body>
+</html>
