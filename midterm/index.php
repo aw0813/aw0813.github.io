986f1d8d9c1888e0260a9bf36f4e679fe567158f
aw0813 <aw0813@naver.com>
2020-10-31 18:05:39 +0000

    mod index

--- midterm/index.php
+++ midterm/index.php
@@ -3,13 +3,19 @@
 <head>
     <meta charset="utf-8">
     <title> World DataBase </title>
+    <style type="text/css" >
+    html{height: 100%;}
+    body{text-align : center; position: relative; width: 100%; height: 100%;}
+    #wrapper {position: absolute; top: 50%; left: 50%; margin:-150px 0 0 -150px}
+    </style>
 </head>
 <body>
+    <div id="wrapper">
     <h1>세계 정보</h1>
 
     <a href = "country.php">(1) 대륙별 국가 정보</a> <br>
     <a href = "city.php">(2) 국가별 도시와 도시의 인구수</a> <br>
     <a href = "language.php">(3) 국가별 사용 언어</a> <br>
-   
+    </div>
 </body>
 </html>
