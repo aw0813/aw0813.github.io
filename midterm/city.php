97b5a208d9f084885a38bdc58ee679ffcaeea7af
aw0813 <aw0813@naver.com>
2020-10-31 15:47:11 +0000

    add city.php

--- midterm/city.php
+++ midterm/city.php
@@ -1,16 +1,18 @@
 <?php 
     $link = mysqli_connect('localhost', 'admin', 'admin', 'world');
     $query = "SELECT * from city WHERE countrycode='kor'";
-    // if(isset($_POST['emp_no'])){
-    //     $filtered_id = mysqli_real_escape_string($link, $_POST['emp_no']);
-    //     $query = " SELECT * FROM employees WHERE emp_no <='{$filtered_id}' order by emp_no desc limit 10";
-    // }
+
+    $city = '';
+
+    if(isset($_GET['code'])){
+        $filtered_city = mysqli_real_escape_string($link, $_GET['code']);
+        $query = " SELECT * from city WHERE countrycode='{$filtered_city}'";
+    }
 
 
 
     $result = mysqli_query($link, $query);
-    
-    $city = '';
+
     while($row = mysqli_fetch_array($result)){
         $city .= '<tr>';
         $city .= '<td>'.$row['Name'].'</td>';
@@ -29,19 +31,38 @@
 <body>
     <h2><a href ="index.php">세계 정보</a> | 도시별 인구수</h2>
 
-    <!-- <form action = "emp_select.php" method = "POST">
-        <label>직원 번호 : </label>
-        <input type="text" name="emp_no" placeholder="직원 번호">
+    <!-- <form action = "city.php" method = "POST">
+        <select name="country">
+            <option value="">국가 선택</option>
+            <option value="<?= $code ?>"><?= $country ?></option>
+        </select>
         <button type="submit">확인</button>
-        <br>
     </form> -->
+
+    <form action = "city.php" method = "GET">
+        <select name="code">   
+            <option value="">국가 선택</option>
+            <?php
+                    $sql = "SELECT DISTINCT a.Name, a.Code FROM country a
+                    LEFT JOIN city b
+                            ON a.Code = b.CountryCode;";
+                    $result = mysqli_query($link, $sql);
+                    while ($row = mysqli_fetch_array($result)){
+                        print "<option value='".$row['Code']."' ";
+                    
+                        print ">".$row['Name']."</option>\n";
+                    }
+            ?>
+        </select>
+        <button type="submit">확인</button>
+    </form>
 
     <br>
     <table border=1 cellspacing=0 cellpadding=5 style="text-align:center;">
         <tr>
             <th>도시명</th>
             <th>인구수</th>
-            <th>행정 구역/th>
+            <th>행정 구역</th>
         </tr>
         <?= $city ?>
 
